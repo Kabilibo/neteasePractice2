092f9133062b9fa65fee1b2260aac37320ac45ff
Brandon Wong <DoubleDou123@outlook.com>
2016-09-05 16:49:36 +0800

    add multiply()

--- gitTry2.java
+++ gitTry2.java
@@ -4,4 +4,12 @@ public class gitTry2 {
 	public static int plus(int a, int b){
 		return a+b;
 	}
+	
+	public static int subtract(int a, int b){
+		return a-b;
+	}
+	
+	public static int multiply(int a, int b){
+		return a*b;
+	}
 }
